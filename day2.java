3820a323ac519e2ae83db810b79d3c8949699fc1
1ansh <anshbansal8788@gmail.com>
2026-01-22 00:43:35 +0530

    day 2 java math, strings and concatenation

---
+++ day2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class day2 {
+    public static void main(String args[]){
+        String fname = "ansh";
+        String lname = "bansal";
+        double num = Math.pow(2,8);
+        System.out.println(fname+lname);
+        System.out.println(fname.length());
+        System.out.println(fname.toUpperCase());
+        System.out.println(fname.toLowerCase());
+        System.out.println(num);
+        System.out.println(Math.sqrt(64));
+        
+        
+
+
+    }
+    
+}
