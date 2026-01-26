e3fc796457423cb475513cb2a577730fc2b662c4
1ansh <anshbansal9900@gmail.com>
2026-01-26 19:21:25 +0530

    check weather its prime or not

---
+++ day6.java
@@ -0,0 +1,46 @@
+// import java.util.*;
+// public class day6 {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         boolean is_prime = true;
+//         for(int i = 2;i<Math.sqrt(num);i++){
+//             if(num%i==0){
+//                 is_prime = false;
+//             }
+//         } 
+//         if(is_prime==true){
+//             System.out.println("n is prime");
+//         }
+//         else{
+//             System.out.println("n is not prime");
+//         }
+//     }
+// }
+//////////////////////////////////////////////////////////
+// public class day6 {
+//     public static void printhello(){
+//         System.out.println("hello world");
+//         System.out.println("how are you");
+//     }
+//     public static void main(String args[]){
+//         printhello();
+//         System.out.println("op");
+//         printhello();
+//     }
+// }
+///////////////////////////////////////////////
+// import java.util.*;
+// public class day6 {
+//     public static void printsum(int a,int b){
+//         int sum = a+b;
+//         System.out.println(sum);      
+//     }
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         printsum(a,b);
+    
+//     }
+// }
