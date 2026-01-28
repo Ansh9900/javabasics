1b3cfb608f0250b47c4d32c85cd7f73dc1def0c6
1ansh <anshbansal9900@gmail.com>
2026-01-28 22:41:17 +0530

    functions

--- day8.java
+++ day8.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 public class day8 {
+    public static int sum(int a , int b , int c){
+        return a+b+c;
+    }
+    public static int sum(int a,int b){
+        return a*b;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("hi day 8 ");
-        
+        int a ,b,c;
+        a = 2;
+        b = 3;
+        c = 2;
+        int result = sum(a, b);
+        int res2 = sum(a,b,c);
     }
 }
