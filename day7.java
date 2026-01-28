d02b0efa6a70f308e4199dcadb033768546150f2
1ansh <anshbansal9900@gmail.com>
2026-01-28 21:40:13 +0530

    function complete

--- day7.java
+++ day7.java
@@ -27,28 +27,29 @@
 //     }
 // }
 //////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class day7 {
-    public static int fact(int n){
-        int factorial=1;
-        for(int i=1;i<=n;i++){
-            factorial=factorial*i;
-        }
-        return factorial;}
-    public static int bincoff(int n,int r){
-        int fact_n = fact(n);
-        int fact_r = fact(r);
-        int fact_nr = fact(n-r);
-        int bcoff = fact_n/(fact_r*fact_nr);
-        return bcoff;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int result = bincoff(a ,b);
+// public class day7 {
+//     public static int fact(int n){
+//         int factorial=1;
+//         for(int i=1;i<=n;i++){
+//             factorial=factorial*i;
+//         }
+//         return factorial;}
+//     public static int bincoff(int n,int r){
+//         int fact_n = fact(n);
+//         int fact_r = fact(r);
+//         int fact_nr = fact(n-r);
+//         int bcoff = fact_n/(fact_r*fact_nr);
+//         return bcoff;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int result = bincoff(a ,b);
         
-        System.out.println(result);
-    }
-}
+//         System.out.println(result);
+//     }
+// }
+////////////////////////////////////////////////////////////////////////
