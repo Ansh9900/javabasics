240c3de7c1cf4ce73bcca8a9b109f1ee23f0b675
1ansh <anshbansal9900@gmail.com>
2026-01-29 23:56:54 +0530

    scope and therotical concept

--- day8.java
+++ day8.java
@@ -1,19 +1,41 @@
+// import java.util.Scanner;
+
+// public class day8 {
+//     public static int sum(int a , int b , int c){
+//         return a+b+c;
+//     }
+//     public static int sum(int a,int b){
+//         return a*b;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a ,b,c;
+//         a = 2;
+//         b = 3;
+//         c = 2;
+//         int result = sum(a, b);
+//         int res2 = sum(a,b,c);
+//     }
+// }
 import java.util.Scanner;
 
 public class day8 {
-    public static int sum(int a , int b , int c){
-        return a+b+c;
-    }
-    public static int sum(int a,int b){
-        return a*b;
+    public static boolean ch_prime(int n){
+        if (n==2) {
+            return false;
+        }
+        else{
+            for(int i = 2 ; i<=Math.sqrt(n);i++){
+                if (n%i==0){
+                    return false;
+                }
+            }
+        }
+        return true; 
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a ,b,c;
-        a = 2;
-        b = 3;
-        c = 2;
-        int result = sum(a, b);
-        int res2 = sum(a,b,c);
+      Scanner sc = new Scanner(System.in);  
+        int n = sc.nextInt();
+        System.out.println(ch_prime(n));
     }
 }
---
+++ day9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class day9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // block of code and scope 
+        
+
+
+
+    }
+}
+
