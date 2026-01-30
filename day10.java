248bc5598d792ebe734c8253b1b53ef6fac9bc81
1ansh <anshbansal9900@gmail.com>
2026-01-30 23:44:30 +0530

    starting

---
+++ day10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class day10 {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     //decimal to binary
+     
+
+
+
+
+
+    }
+}
