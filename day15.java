a4f38f46e3c01f7b451e60fcd60cb68e4859479a
1ansh <anshbansal9900@gmail.com>
2026-02-05 23:29:50 +0530

    python

--- day15.java
+++ day15.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class day15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
-    
-    
+        int a = 97;
+        char b = a;
+        int c = 5+b;
+        System.out.println(a+"\n"+b+"\n"+ c);
     
     
     
