25aae1fbdc9f8fae679f775ee47b6c7fc35b10e7
1ansh <anshbansal9900@gmail.com>
2026-02-07 23:05:34 +0530

    practice day1

---
+++ day18.java
@@ -0,0 +1,41 @@
+// import java.util.Scanner;
+// public class day18 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n;
+//         n = sc.nextInt();
+//         for(int i = 2;i<=n/2;i++){
+//             if(n%i==0){
+//                 System.out.println("not prime");
+//                 break;
+//             }
+//             else{
+//                 System.out.println("prime");
+//                 break;
+//             }
+
+//         }
+//     }
+// }
+import java.util.Scanner;
+
+public class day18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        int rev = 0;
+        while (n!=0) {
+        int d = n%10;
+        rev = rev * 10+d;
+        n=n/10;
+        i++;
+        }
+    System.out.println(rev);        
+
+
+
+
+
+    }
+}
