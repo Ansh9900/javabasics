9ab2edddaeffa1f8625041e6703aa3aae64bb0ff
1ansh <anshbansal9900@gmail.com>
2026-02-09 23:45:27 +0530

    practice day4

---
+++ day20.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class day20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("practice day 3");
+
+
+    }
+}
