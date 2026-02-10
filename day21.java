53980fa974426937f0ce891bb20f84637ee86b62
1ansh <anshbansal9900@gmail.com>
2026-02-10 23:45:21 +0530

    prac day4

---
+++ day21.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class day21 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
